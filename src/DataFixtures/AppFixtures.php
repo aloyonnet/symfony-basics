701f153182ebdbf12cd9f7036f2a82c81034c7ce
Alexis Loyonnet <alexis.loyonnet@gmail.com>
2025-11-03 15:28:07 +0100

    Ajout des Fixtures pour l'entité Post

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
+use App\Entity\Post;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -18,13 +19,36 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $this->loadUsers($manager);
+        $this->loadPosts($manager);
+    }
+
+    public function loadUsers(ObjectManager $manager): void
+    {
         foreach ($this->getUserData() as [$username, $password, $roles]) {
         $user = (new User());
         $user->setUsername($username)
             ->setPassword($this->hasher->hashPassword($user, $password))
             ->setRoles($roles);
 
-            $manager->persist($user);
+        $manager->persist($user);
+
+        $this->addReference($username, $user);
+        }
+
+        $manager->flush();
+    }
+
+    public function loadPosts(ObjectManager $manager): void
+    {
+        foreach ($this->getPostData() as [$title, $slug, $content, $author]) {
+        $post = (new Post());
+        $post->setTitle($title)
+            ->setSlug($slug)
+            ->setContent($content)
+            ->setAuthor($author);
+
+        $manager->persist($post);
         }
 
         $manager->flush();
@@ -39,6 +63,19 @@ class AppFixtures extends Fixture
             // [$username, $password, $role];
             ['user', 'test', ['ROLE_USER']],
             ['admin', 'test', ['ROLE_ADMIN']],
+        ];
+    }
+
+        /**
+     * @return array|array[]
+     */
+    private function getPostData(): array
+    {
+        return [
+            // [$title, $slug, $content, $author];
+            ['Article 1', 'article-1', 'Contenu de l\'article 1', $this->getReference('admin', User::class)],
+            ['Article 2', 'article-2',  'Contenu de l\'article 2', $this->getReference('admin', User::class)],
+            ['Article 3', 'article-3',  'Contenu de l\'article 3', $this->getReference('admin', User::class)],
         ];
     }
 }
